2f7b1373a549364de76f9b69713504c7061cad5e
tjswar <tejaswar8484@gmail.com>
2025-10-21 14:46:42 -0700

    Update config.py to support Streamlit Cloud secrets

--- config.py
+++ config.py
@@ -1,5 +1,24 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-GEMINI_KEY = os.getenv("GEMINI_API_KEY")
+
+GEMINI_KEY = None
+
+
+try:
+    import streamlit as st
+    GEMINI_KEY = st.secrets["GEMINI_API_KEY"]
+except:
+    pass
+
+
+if not GEMINI_KEY:
+    GEMINI_KEY = os.getenv("GEMINI_API_KEY")
+
+
+if not GEMINI_KEY:
+    try:
+        from dotenv import load_dotenv
+        load_dotenv()
+        GEMINI_KEY = os.getenv("GEMINI_API_KEY")
+    except:
+        pass
